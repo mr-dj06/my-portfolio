dc7fa49247be42b12c59ee37ff63919ae9c49b2d
mr-dj06 <112109506+mr-dj06@users.noreply.github.com>
2024-06-04 22:05:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,28 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+window.history.replaceState('','','/');
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelectorAll('a','button').forEach(function(link) {
+      link.addEventListener('click', function(event) {
+          event.preventDefault(); // Prevent the default link behavior
+
+          var href = this.getAttribute('href');
+          var xhr = new XMLHttpRequest();
+          xhr.open('GET', href);
+          xhr.onload = function() {
+              if (xhr.status === 404) {
+                  window.location.href = '404.html'; // Redirect to custom error page
+              } else {
+                  window.location.href = href; // Proceed to the link if it exists
+              }
+          };
+          xhr.onerror = function() {
+              console.error('Error checking the link.');
+          };
+          xhr.send();
+      });
+  });
+});
